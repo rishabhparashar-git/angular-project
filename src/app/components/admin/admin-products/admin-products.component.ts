ccbe6f8ccb1fa2f44cbcd80e8571c7d197987ef6
Rishabh Parashar <parashar040601@gmail.com>
2022-11-14 15:46:24 +0530

    added filter and search functionality

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminProductsService } from 'src/app/services/admin/admin-products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Product } from 'src/app/services/products/product';
 
 @Component({
   selector: 'app-admin-products',
@@ -10,6 +11,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class AdminProductsComponent implements OnInit {
   availableProducts: any;
+  localProductList: Product[] = [];
+  category: string | null = null;
   constructor(
     private _snackBar: MatSnackBar,
     private router: Router,
@@ -19,8 +22,18 @@ export class AdminProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((resp) => {
-      this.availableProducts = resp;
-      console.log(resp);
+      this.localProductList = resp;
+      this.route.queryParamMap.subscribe((params) => {
+        let category = params.get('category');
+        if (category) {
+          console.log(category);
+          this.availableProducts = this.localProductList.filter(
+            (prod: any) => prod.category === category
+          );
+        } else {
+          this.availableProducts = [...this.localProductList];
+        }
+      });
     });
   }
 
@@ -37,6 +50,22 @@ export class AdminProductsComponent implements OnInit {
   triggerSnackBar(message: any) {
     this._snackBar.open(message, 'OK', {
       duration: 3000,
+    });
+  }
+
+  search(query: string) {
+    this.availableProducts = query
+      ? this.localProductList.filter((prod: Product) =>
+          prod.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.localProductList;
+  }
+
+  changeCategory(categoryToFilterBy: string) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams:
+        categoryToFilterBy === 'clear' ? {} : { category: categoryToFilterBy },
     });
   }
 }
