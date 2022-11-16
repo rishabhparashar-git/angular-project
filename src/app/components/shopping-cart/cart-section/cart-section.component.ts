2fca864d251040989a780edd1094b47ba3d247c9
Rishabh Parashar <parashar040601@gmail.com>
2022-11-16 10:59:03 +0530

    added dynamic content to cart section
    There is a bug, on changing the items in cart, the cart is actually updating in the service but it is not reflecting in the component which is using it in this case the Cart-Section-Component !!

--- src/app/components/shopping-cart/cart-section/cart-section.component.ts
+++ src/app/components/shopping-cart/cart-section/cart-section.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart/cart.service';
+import { Product } from 'src/app/services/products/product';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-cart-section',
   templateUrl: './cart-section.component.html',
-  styleUrls: ['./cart-section.component.css']
+  styleUrls: ['./cart-section.component.css'],
 })
 export class CartSectionComponent implements OnInit {
-
-  constructor() { }
+  productsList: any[] = [];
+  totalAmount: number = 0;
+  gst: number = 0;
+  payableAmount: number = 0;
+  constructor(
+    private productService: ProductsService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
+    let cartInfo = this.cartService.cart;
+    this.productService.getProducts().subscribe((resp: Product[]) => {
+      let allProducts = resp;
+      allProducts.forEach((prod) => {
+        let id: string = prod.id ? prod.id : '';
+        if (Object.keys(cartInfo).includes(id)) {
+          let currTotal = +prod.price * cartInfo[id];
+          this.productsList.push({
+            ...prod,
+            count: cartInfo[id],
+            total: currTotal,
+          });
+          this.totalAmount += currTotal;
+        }
+      });
+      this.gst = Math.floor(18 * (this.totalAmount / 100));
+      this.payableAmount = this.gst + this.totalAmount;
+      console.log(this.productsList);
+    });
   }
 
+  decrement(id: string) {
+    this.cartService.cartOperations(id, '-');
+  }
+  increment(id: string) {
+    this.cartService.cartOperations(id, '+');
+  }
 }
